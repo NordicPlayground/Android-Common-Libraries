72d9d278bb9e7e9ae9371ec1e651df36790d42c0
Sylwester Zieliński <sylwester.zielinski@nordicsemi.no>
2022-02-24 16:19:23 +0100

    Move service module from Toolbox

---
+++ service/src/main/java/no/nordicsemi/android/service/BleManagerStatus.kt
@@ -0,0 +1,24 @@
+package no.nordicsemi.android.service
+
+sealed class BleManagerResult <T> {
+
+    fun isRunning(): Boolean {
+        return this is SuccessResult
+    }
+
+    fun hasBeenDisconnected(): Boolean {
+        return this is LinkLossResult || this is DisconnectedResult || this is MissingServiceResult
+    }
+
+    fun hasBeenDisconnectedWithoutLinkLoss(): Boolean {
+        return this is DisconnectedResult || this is MissingServiceResult
+    }
+}
+
+class ConnectingResult<T> : BleManagerResult<T>()
+data class SuccessResult<T>(val data: T) : BleManagerResult<T>()
+
+class LinkLossResult<T>(val data: T) : BleManagerResult<T>()
+class DisconnectedResult<T> : BleManagerResult<T>()
+class UnknownErrorResult<T> : BleManagerResult<T>()
+class MissingServiceResult<T> : BleManagerResult<T>()
