143ecedbaf11160e20fa67d4719348657d202a05
Sylwester Zieliński <sylwester.zielinski@nordicsemi.no>
2022-07-26 14:18:48 +0200

    Extract ProgressItem

---
+++ theme/src/main/java/no/nordicsemi/android/theme/view/ProgressItem.kt
@@ -0,0 +1,71 @@
+package no.nordicsemi.android.theme.view
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import no.nordicsemi.android.theme.R
+
+enum class ProgressItemStatus {
+    DISABLED, WORKING, SUCCESS, ERROR
+}
+
+@Composable
+fun ProgressItem(text: String, status: ProgressItemStatus) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            painter = painterResource(status.toImageRes()),
+            contentDescription = stringResource(id = R.string.progress_icon),
+            tint = status.toIconColor()
+        )
+        Spacer(modifier = Modifier.size(8.dp))
+        Text(
+            text = text,
+            style = MaterialTheme.typography.bodyMedium,
+            color = status.toTextColor(),
+            modifier = Modifier.padding(start = 16.dp)
+        )
+    }
+}
+
+@Composable
+private fun ProgressItemStatus.toIconColor(): Color {
+    return when (this) {
+        ProgressItemStatus.DISABLED -> MaterialTheme.colorScheme.surfaceVariant
+        ProgressItemStatus.WORKING -> MaterialTheme.colorScheme.onBackground
+        ProgressItemStatus.SUCCESS -> colorResource(id = R.color.nordicGrass)
+        ProgressItemStatus.ERROR -> MaterialTheme.colorScheme.error
+    }
+}
+
+@Composable
+private fun ProgressItemStatus.toTextColor(): Color {
+    return when (this) {
+        ProgressItemStatus.DISABLED -> MaterialTheme.colorScheme.surfaceVariant
+        ProgressItemStatus.WORKING,
+        ProgressItemStatus.SUCCESS,
+        ProgressItemStatus.ERROR -> MaterialTheme.colorScheme.onBackground
+    }
+}
+
+@DrawableRes
+private fun ProgressItemStatus.toImageRes(): Int {
+    return when (this) {
+        ProgressItemStatus.DISABLED -> R.drawable.ic_dot
+        ProgressItemStatus.WORKING -> R.drawable.ic_arrow_right
+        ProgressItemStatus.SUCCESS -> R.drawable.ic_check
+        ProgressItemStatus.ERROR -> R.drawable.ic_cross
+    }
+}
