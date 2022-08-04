f78931e7fecc63d5e2a322baaf3388eec42f5da1
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2022-08-04 14:04:19 +0200

    Minor refactoring

--- uiscanner/src/main/java/no/nordicsemi/android/common/ui/scanner/ProvisioningData.kt
+++ uiscanner/src/main/java/no/nordicsemi/android/common/ui/scanner/ProvisioningData.kt
@@ -11,19 +11,18 @@ data class ProvisioningData(
     val rssi: Int
 ) {
     constructor(data: ByteArray) : this(
-        data[0].toInt(),
-        data[1].toInt() and 0x01 != 0,
-        data[1].toInt() and 0x02 != 0,
-        data[3].toInt()
+        version = data[0].toInt(),
+        // 2 bytes reserved for flags
+        isProvisioned = data[1].toInt() and 0x01 != 0,
+        isConnected = data[1].toInt() and 0x02 != 0,
+        rssi = data[3].toInt()
     )
 
     companion object {
         private val parcelUuid = ParcelUuid(UUID.fromString("14387800-130c-49e7-b877-2881c89cb258"))
 
-        fun create(scanResult: ScanResult): ProvisioningData? {
-            return scanResult.scanRecord?.serviceData?.get(parcelUuid)?.let {
-                ProvisioningData(it)
-            }
-        }
+        fun create(scanResult: ScanResult) = scanResult.scanRecord?.serviceData
+            ?.get(parcelUuid)
+            ?.let { ProvisioningData(it) }
     }
 }
