790ee3de5a7c09f2384eb5a98bac612b6df17d30
Sylwester Zieliński <sylwester.zielinski@nordicsemi.no>
2022-08-31 10:25:00 +0200

    Add Wi-Fi icon to RSSI

--- theme/src/main/java/no/nordicsemi/android/common/theme/view/RssiIcon.kt
+++ theme/src/main/java/no/nordicsemi/android/common/theme/view/RssiIcon.kt
@@ -3,10 +3,15 @@ package no.nordicsemi.android.common.theme.view
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.NetworkWifi
+import androidx.compose.material.icons.filled.NetworkWifi1Bar
+import androidx.compose.material.icons.filled.NetworkWifi3Bar
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import no.nordicsemi.android.common.theme.R
@@ -31,9 +36,17 @@ fun RssiIcon(rssi: Int) {
 
 @DrawableRes
 private fun getImageRes(rssi: Int): Int {
-    return when  {
+    return when {
         rssi < MEDIUM_RSSI -> R.drawable.ic_signal_min
         rssi < MAX_RSSI -> R.drawable.ic_signal_medium
         else -> R.drawable.ic_signal_max
+    }
+}
+
+fun getWiFiRes(rssi: Int): ImageVector {
+    return when {
+        rssi < MEDIUM_RSSI -> Icons.Default.NetworkWifi1Bar
+        rssi < MAX_RSSI -> Icons.Default.NetworkWifi3Bar
+        else -> Icons.Default.NetworkWifi
     }
 }
