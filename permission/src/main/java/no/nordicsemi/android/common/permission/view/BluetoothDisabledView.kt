ab58c54f3031b7d04dca5afdba9ba051d9e42230
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2022-09-08 16:49:30 +0200

    Unused imports

--- permission/src/main/java/no/nordicsemi/android/common/permission/view/BluetoothDisabledView.kt
+++ permission/src/main/java/no/nordicsemi/android/common/permission/view/BluetoothDisabledView.kt
@@ -34,10 +34,7 @@ package no.nordicsemi.android.common.permission.view
 import android.bluetooth.BluetoothAdapter
 import android.content.Context
 import android.content.Intent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
@@ -45,12 +42,10 @@ import androidx.compose.material.icons.filled.BluetoothDisabled
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import no.nordicsemi.android.common.permission.R
 import no.nordicsemi.android.common.theme.view.WarningView
 
