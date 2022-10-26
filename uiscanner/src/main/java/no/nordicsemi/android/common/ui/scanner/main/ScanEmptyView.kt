054b0149d698a9e17b2c64b6518e76223abb8267
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2022-10-26 13:12:21 +0200

    Bug fix: ScanEmptyView not centered

--- uiscanner/src/main/java/no/nordicsemi/android/common/ui/scanner/main/ScanEmptyView.kt
+++ uiscanner/src/main/java/no/nordicsemi/android/common/ui/scanner/main/ScanEmptyView.kt
@@ -34,6 +34,7 @@ package no.nordicsemi.android.common.ui.scanner.main
 import android.content.Context
 import android.content.Intent
 import android.provider.Settings
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.BluetoothSearching
@@ -44,6 +45,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import no.nordicsemi.android.common.core.parseBold
@@ -56,7 +58,9 @@ internal fun ScanEmptyView(
     requireLocation: Boolean,
 ) {
     WarningView(
-        modifier = Modifier.padding(16.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
         imageVector = Icons.Default.BluetoothSearching,
         title = stringResource(id = R.string.no_device_guide_title),
         hint = stringResource(id = R.string.no_device_guide_info) + if (requireLocation) {
@@ -91,7 +95,7 @@ private fun ScanEmptyViewPreview_RequiredLocation() {
     }
 }
 
-@Preview
+@Preview(device = Devices.TABLET)
 @Composable
 private fun ScanEmptyViewPreview() {
     NordicTheme {
