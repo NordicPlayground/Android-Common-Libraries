3f26a14172875b81ab88aa2f0379db3bfb7341e4
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2022-11-16 10:06:20 +0100

    Unused dependency removed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,6 @@ android {
 }
 
 dependencies {
-    implementation(project(":analytics"))
     implementation(project(":theme"))
     implementation(project(":uilogger"))
     implementation(project(":uiscanner"))
