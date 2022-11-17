e8853692da4eb89aa5ab2eb620e374347d388905
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2022-11-17 15:54:31 +0100

    Commons dependencies fixed

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,17 +14,17 @@
  * limitations under the License.
  */
 plugins {
-    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.jvm) apply false
-    alias(libs.plugins.kotlin.serialization) apply false
+    //alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.hilt) apply false
-    alias(libs.plugins.secrets) apply false
-    alias(libs.plugins.protobuf) apply false
-    alias(libs.plugins.nordic.application) apply false
+    //alias(libs.plugins.secrets) apply false
+    //alias(libs.plugins.protobuf) apply false
+    //alias(libs.plugins.nordic.application) apply false
     alias(libs.plugins.nordic.application.compose) apply false
-    alias(libs.plugins.nordic.library) apply false
+    //alias(libs.plugins.nordic.library) apply false
     alias(libs.plugins.nordic.library.compose) apply false
     alias(libs.plugins.nordic.hilt) apply false
-    alias(libs.plugins.nordic.feature) apply false
+    //alias(libs.plugins.nordic.feature) apply false
     alias(libs.plugins.nordic.nexus) apply false
 }
--- permission/build.gradle.kts
+++ permission/build.gradle.kts
@@ -1,6 +1,4 @@
 plugins {
-    alias(libs.plugins.nordic.feature)
-    alias(libs.plugins.nordic.library)
     alias(libs.plugins.nordic.library.compose)
     alias(libs.plugins.nordic.hilt)
     alias(libs.plugins.nordic.nexus)
@@ -35,13 +33,8 @@ dependencies {
     implementation(project(":theme"))
     implementation(project(":navigation"))
 
-    implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.material.iconsExtended)
 
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    debugImplementation(libs.androidx.compose.ui.tooling)
-
-    implementation(libs.hilt.android)
+    implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.hilt.navigation.compose)
-    kapt(libs.hilt.compiler)
 }
