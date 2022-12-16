b962edf844ace954524dcf88ce25b03d9522d3bb
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2022-12-16 16:38:29 +0100

    Migration to Version Catalog 1.1.2 and Nordic plugins 1.1.1

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -39,13 +39,16 @@ group = "no.nordicsemi.android.common"
 nordicNexusPublishing {
     POM_ARTIFACT_ID = "core"
     POM_NAME = "Core Nordic common library."
-    GROUP = "no.nordicsemi.android.common"
 
     POM_DESCRIPTION = "Nordic Android Common Libraries"
     POM_URL = "https://github.com/NordicPlayground/Android-Common-Libraries"
     POM_SCM_URL = "https://github.com/NordicPlayground/Android-Common-Libraries"
     POM_SCM_CONNECTION = "scm:git@github.com:NordicPlayground/Android-Common-Libraries.git"
     POM_SCM_DEV_CONNECTION = "scm:git@github.com:NordicPlayground/Android-Common-Libraries.git"
+
+    POM_DEVELOPER_ID = "syzi"
+    POM_DEVELOPER_NAME = "Sylwester Zieliński"
+    POM_DEVELOPER_EMAIL = "sylwester.zielinski@nordicsemi.no"
 }
 
 android {
--- uilogger/build.gradle.kts
+++ uilogger/build.gradle.kts
@@ -40,13 +40,16 @@ group = "no.nordicsemi.android.common"
 nordicNexusPublishing {
     POM_ARTIFACT_ID = "uilogger"
     POM_NAME = "Nordic library for library for UI helpers utilizing nordic logger library."
-    GROUP = "no.nordicsemi.android.common"
 
     POM_DESCRIPTION = "Nordic Android Common Libraries"
     POM_URL = "https://github.com/NordicPlayground/Android-Common-Libraries"
     POM_SCM_URL = "https://github.com/NordicPlayground/Android-Common-Libraries"
     POM_SCM_CONNECTION = "scm:git@github.com:NordicPlayground/Android-Common-Libraries.git"
     POM_SCM_DEV_CONNECTION = "scm:git@github.com:NordicPlayground/Android-Common-Libraries.git"
+
+    POM_DEVELOPER_ID = "syzi"
+    POM_DEVELOPER_NAME = "Sylwester Zieliński"
+    POM_DEVELOPER_EMAIL = "sylwester.zielinski@nordicsemi.no"
 }
 
 android {
