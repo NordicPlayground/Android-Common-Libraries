d23038b08dc2f1a5ac9b291983bff56e47bb5d9a
roshanrajaratnam <rajaratnam.roshan@gmail.com>
2024-06-17 10:36:38 +0200

    Fix imports

--- theme/src/main/java/no/nordicsemi/android/common/theme/view/NordicMediumAppBar.kt
+++ theme/src/main/java/no/nordicsemi/android/common/theme/view/NordicMediumAppBar.kt
@@ -35,12 +35,18 @@ import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material3.*
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MediumTopAppBar
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextOverflow
 import no.nordicsemi.android.common.theme.R
 
 @ExperimentalMaterial3Api
