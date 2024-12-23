cead81cc6caadeac7e782fd4955032ab2e077909
Aleksander Nowakowski <aleksander.nowakowski@nordicsemi.no>
2024-12-23 22:30:49 +0100

    Enabling Dokka V2

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,6 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.kotlin.parcelize) apply false
-    alias(libs.plugins.kotlin.dokka) apply false
     alias(libs.plugins.compose.compiler) apply false
 
     // Nordic plugins are defined in https://github.com/NordicSemiconductor/Android-Gradle-Plugins
@@ -43,4 +42,10 @@ plugins {
     alias(libs.plugins.nordic.hilt) apply false
     alias(libs.plugins.nordic.nexus.android) apply false
     alias(libs.plugins.nordic.kotlin.android) apply false
+
+    // This plugin is used to generate Dokka documentation.
+    alias(libs.plugins.kotlin.dokka) apply false
+    // This applies Nordic look & feel to generated Dokka documentation.
+    // https://github.com/NordicSemiconductor/Android-Gradle-Plugins/blob/main/plugins/src/main/kotlin/NordicDokkaPlugin.kt
+    alias(libs.plugins.nordic.dokka) apply true
 }
