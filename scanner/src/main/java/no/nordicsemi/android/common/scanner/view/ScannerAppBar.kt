58da965e56544b635cbe9d1ecf22dac6e45c6d75
hiar <aryal.himali@gmail.com>
2025-05-09 14:08:08 +0200

    Scanning filter ui

--- scanner/src/main/java/no/nordicsemi/android/common/scanner/view/ScannerAppBar.kt
+++ scanner/src/main/java/no/nordicsemi/android/common/scanner/view/ScannerAppBar.kt
@@ -31,16 +31,23 @@
 
 package no.nordicsemi.android.common.scanner.view
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import no.nordicsemi.android.common.ui.view.NordicAppBar
 
@@ -57,14 +64,36 @@ internal fun ScannerAppBar(
         backButtonIcon = backButtonIcon,
         onNavigationButtonClick = onNavigationButtonClick,
         actions = {
-            if (showProgress) {
-                CircularProgressIndicator(
+            Row {
+                if (showProgress) {
+                    CircularProgressIndicator(
+                        modifier = Modifier
+                            .padding(horizontal = 16.dp)
+                            .size(30.dp),
+                        color = MaterialTheme.colorScheme.onPrimary
+                    )
+                }
+                Icon(
+                    imageVector = Icons.Default.MoreVert,
+                    contentDescription = null,
                     modifier = Modifier
-                        .padding(horizontal = 16.dp)
-                        .size(30.dp),
-                    color = MaterialTheme.colorScheme.onPrimary
+                        .clip(CircleShape)
+                        .clickable {
+
+                        }
+                        .padding(4.dp)
                 )
             }
         },
+    )
+}
+
+@Preview
+@Composable
+private fun ScannerAppBarPreview() {
+    ScannerAppBar(
+        title = { },
+        showProgress = false,
+        onNavigationButtonClick = { }
     )
 }
