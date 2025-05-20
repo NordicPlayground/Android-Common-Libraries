4c2a14e88d9a4e2b5d027a2fe82358c6c946207b
hiar <aryal.himali@gmail.com>
2025-05-20 13:34:27 +0200

    scanning error states

--- scanner/src/main/java/no/nordicsemi/android/common/scanner/data/ScanningStateError.kt
+++ scanner/src/main/java/no/nordicsemi/android/common/scanner/data/ScanningStateError.kt
@@ -1,30 +1,70 @@
 package no.nordicsemi.android.common.scanner.data
 
-enum class ScanningStateError (val errorCode: Int){
-    /** No error. */
-    NO_ERROR(0),
+import android.bluetooth.le.ScanCallback
+import android.os.Build
+import androidx.annotation.RequiresApi
 
-    /** Bluetooth is not supported on this device. */
-    BLUETOOTH_NOT_SUPPORTED(1),
+/**
+ * Reason for failed scan.
+ *
+ * @property value Native Android API value.
+ */
+enum class ScanFailedError(val value: Int) {
 
-    /** Bluetooth is disabled. */
-    BLUETOOTH_DISABLED(2),
+    /**
+     * Fails to start scan as BLE is disabled on the device.
+     */
+    BLUETOOTH_DISABLED(-1),
 
-    /** Location permission is not granted. */
-    LOCATION_PERMISSION_NOT_GRANTED(3),
+    /**
+     * Helper value representing an unknown error.
+     */
+    UNKNOWN(0),
 
-    /** Location services are disabled. */
-    LOCATION_SERVICES_DISABLED(4),
+    /**
+     * Fails to start scan as BLE scan with the same settings is already started by the app.
+     */
+    SCAN_FAILED_ALREADY_STARTED(ScanCallback.SCAN_FAILED_ALREADY_STARTED),
 
-    /** Scanning is already in progress. */
-    SCANNING_IN_PROGRESS(5),
+    /**
+     * Fails to start scan as app cannot be registered.
+     */
+    SCAN_FAILED_APPLICATION_REGISTRATION_FAILED(ScanCallback.SCAN_FAILED_APPLICATION_REGISTRATION_FAILED),
 
-    /** Scanning has been stopped. */
-    SCANNING_STOPPED(6);
+    /**
+     * Fails to start power optimized scan as this feature is not supported.
+     */
+    SCAN_FAILED_FEATURE_UNSUPPORTED(ScanCallback.SCAN_FAILED_FEATURE_UNSUPPORTED),
+
+    /**
+     * Fails to start scan due an internal error.
+     */
+    SCAN_FAILED_INTERNAL_ERROR(ScanCallback.SCAN_FAILED_INTERNAL_ERROR),
+
+    /**
+     * Fails to start scan as it is out of hardware resources.
+     */
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    SCAN_FAILED_OUT_OF_HARDWARE_RESOURCES(ScanCallback.SCAN_FAILED_OUT_OF_HARDWARE_RESOURCES),
+
+    /**
+     * Fails to start scan as application tries to scan too frequently.
+     */
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    SCAN_FAILED_SCANNING_TOO_FREQUENTLY(ScanCallback.SCAN_FAILED_SCANNING_TOO_FREQUENTLY);
 
     companion object {
-        fun fromErrorCode(errorCode: Int): ScanningStateError {
-            return entries.firstOrNull { it.errorCode == errorCode } ?: NO_ERROR
+        fun create(value: Int): ScanFailedError {
+            return entries.firstOrNull { it.value == value } ?: UNKNOWN
         }
     }
 }
+
+/**
+ * Exception indicating that BLE scanning failed.
+ *
+ * @property errorCode A detailed error code.
+ */
+data class ScanningFailedException(
+    val errorCode: ScanFailedError,
+) : Exception("Scanning failed with the code: $errorCode")
