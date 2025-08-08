f4581c9982dd7e26a5fa2558b59f6dc8c876997a
hiar <aryal.himali@gmail.com>
2025-08-08 16:44:56 +0200

    Fixed Analytics dialog padding

--- analytics/src/main/java/no/nordicsemi/android/common/analytics/view/AnalyticsPermissionRequestDialog.kt
+++ analytics/src/main/java/no/nordicsemi/android/common/analytics/view/AnalyticsPermissionRequestDialog.kt
@@ -33,18 +33,32 @@
 
 package no.nordicsemi.android.common.analytics.view
 
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import androidx.hilt.navigation.compose.hiltViewModel
 import kotlinx.coroutines.flow.collectLatest
 import no.nordicsemi.android.common.analytics.R
@@ -74,33 +88,55 @@ fun AnalyticsPermissionRequestDialog() {
 }
 
 @Composable
-fun AnalyticsPermissionRequestDialog(
+private fun AnalyticsPermissionRequestDialog(
     onConfirm: () -> Unit,
     onDecline: () -> Unit,
 ) {
-    AlertDialog(
-        onDismissRequest = onDecline,
-        title = { Text(stringResource(id = R.string.analytics_dialog_title)) },
-        text = {
-            Text(
-                text = stringResource(id = R.string.analytics_dialog_info).parseBold(),
-                modifier = Modifier.verticalScroll(rememberScrollState())
-            )
-        },
-        confirmButton = {
-            TextButton(onClick = onConfirm) {
-                Text(stringResource(id = R.string.analytics_dialog_accept))
+    Dialog(onDismissRequest = onDecline) {
+        Surface(
+            modifier = Modifier
+                .wrapContentWidth()
+                .wrapContentHeight(),
+            shape = MaterialTheme.shapes.extraLarge,
+        ) {
+            Column(
+                modifier = Modifier
+                    .verticalScroll(rememberScrollState())
+                    .padding(24.dp) // outer padding
+                    .wrapContentHeight(),
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+            ) {
+                // Title (optional)
+                Text(
+                    stringResource(id = R.string.analytics_dialog_title),
+                    style = MaterialTheme.typography.titleLarge,
+                    color = MaterialTheme.colorScheme.onSurface
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+
+                // Content text
+                Text(
+                    text = stringResource(id = R.string.analytics_dialog_info).parseBold(),
+                    color = MaterialTheme.colorScheme.onSurfaceVariant,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+                // NO spacing between text and buttons here
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.End
+                ) {
+                    TextButton(onClick = onDecline) {
+                        Text(stringResource(id = R.string.analytics_dialog_decline))
+                    }
+                    TextButton(onClick = onConfirm) {
+                        Text(stringResource(id = R.string.analytics_dialog_accept))
+                    }
+                }
             }
-        },
-        dismissButton = {
-            TextButton(onClick = onDecline ) {
-                Text(stringResource(id = R.string.analytics_dialog_decline))
-            }
-        },
-        modifier = Modifier
-            .fillMaxHeight(0.9f)
-            .fillMaxWidth(0.95f)
-    )
+        }
+    }
+
 }
 
 @Preview
